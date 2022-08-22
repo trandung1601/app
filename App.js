4a67f12086c54cc15e69879df47cb3989fa56117
Dung <dung.trantien@vn.bosch.com>
2022-08-22 22:56:41 +0700

    init app

---
+++ App.js
@@ -0,0 +1,23 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Screen/Home';
+import ChooseLocation from './src/Screen/ChooseLocation';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ChooseLocation" component={ChooseLocation} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
